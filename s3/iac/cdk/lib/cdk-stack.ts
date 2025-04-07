3a5d6584540eec9ef044654a75190eb307049ea3
Raimudo Pereira <raimundompereira27@gmail.com>
2025-04-07 16:59:54 +0000

    Simple CDK example

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'MyBucket')
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
